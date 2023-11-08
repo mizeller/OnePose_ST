7f40538940a1afbaaedaa529f691c008b5090e8f
Michel Zeller <mizeller@student.ethz.ch>
2023-11-08 15:25:06 +0100

    fix: demo pipeline + launch.json entrypoints

--- scripts/demo_pipeline.sh
+++ scripts/demo_pipeline.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PROJECT_DIR="$(pwd)"
-# OBJ_NAME=$1
-OBJ_NAME='demo_cam' 
+OBJ_NAME=$1
 echo "Current work dir: $PROJECT_DIR"
 
 echo '-------------------'
@@ -19,11 +18,10 @@ echo '--------------------------------------------------------------'
 python3 $PROJECT_DIR/run.py \
     +preprocess="sfm_demo" \
     dataset.data_dir="[$PROJECT_DIR/data/demo/$OBJ_NAME $OBJ_NAME-annotate]" \
-    dataset.outputs_dir="$PROJECT_DIR/data/demo/sfm_model" \
+    dataset.outputs_dir="$PROJECT_DIR/data/demo/$OBJ_NAME/sfm_model" \
 
-# echo "-----------------------------------"
-# echo "Run inference and output demo video:"
-# echo "-----------------------------------"
-
-# # Run inference on $OBJ_NAME-test and output demo video:
-# python $PROJECT_DIR/demo.py +experiment="inference_demo" data_base_dir="$PROJECT_DIR/data/demo/$OBJ_NAME $OBJ_NAME-test" sfm_base_dir="$PROJECT_DIR/data/demo/sfm_model/outputs_softmax_loftr_loftr/$OBJ_NAME"
+echo "-----------------------------------"
+echo "Run inference and output demo video:"
+echo "-----------------------------------"
+# Run inference on $OBJ_NAME-test and output demo video:
+python3 $PROJECT_DIR/demo.py +experiment="inference_demo" data_base_dir="$PROJECT_DIR/data/demo/$OBJ_NAME $OBJ_NAME-test" sfm_base_dir="$PROJECT_DIR/data/demo/$OBJ_NAME/sfm_model/outputs_softmax_loftr_loftr/$OBJ_NAME"
