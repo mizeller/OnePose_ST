68eee17a9306f2039efe50a02d96095a37e7f81e
Michel Zeller <mizeller@student.ethz.ch>
2023-11-08 15:59:18 +0100

    fix: remove unneccessary variable in demo pipeline, we know the obj name and dont want to reuse this script anyways

--- scripts/demo_pipeline.sh
+++ scripts/demo_pipeline.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 PROJECT_DIR="$(pwd)"
-OBJ_NAME=$1
 echo "Current work dir: $PROJECT_DIR"
 
 echo '-------------------'
@@ -9,19 +8,19 @@ echo '-------------------'
 # Parse scanned annotated & test sequence:
 python3 $PROJECT_DIR/parse_scanned_data.py \
     --scanned_object_path \
-    "$PROJECT_DIR/data/demo/$OBJ_NAME"
+    "$PROJECT_DIR/data/demo/demo_cam"
 
 echo '--------------------------------------------------------------'
 echo 'Run Keypoint-Free SfM to reconstruct object point cloud for pose estimation:'
 echo '--------------------------------------------------------------'
-# Run SfM to reconstruct object sparse point cloud from $OBJ_NAME-annotate sequence:
+# Run SfM to reconstruct object sparse point cloud from demo_cam-annotate sequence:
 python3 $PROJECT_DIR/run.py \
     +preprocess="sfm_demo" \
-    dataset.data_dir="[$PROJECT_DIR/data/demo/$OBJ_NAME $OBJ_NAME-annotate]" \
-    dataset.outputs_dir="$PROJECT_DIR/data/demo/$OBJ_NAME/sfm_model" \
+    dataset.data_dir="[$PROJECT_DIR/data/demo/demo_cam demo_cam-annotate]" \
+    dataset.outputs_dir="$PROJECT_DIR/data/demo/sfm_model" \
 
 echo "-----------------------------------"
 echo "Run inference and output demo video:"
 echo "-----------------------------------"
-# Run inference on $OBJ_NAME-test and output demo video:
-python3 $PROJECT_DIR/demo.py +experiment="inference_demo" data_base_dir="$PROJECT_DIR/data/demo/$OBJ_NAME $OBJ_NAME-test" sfm_base_dir="$PROJECT_DIR/data/demo/$OBJ_NAME/sfm_model/outputs_softmax_loftr_loftr/$OBJ_NAME"
+# Run inference on demo_cam-test and output demo video:
+python3 $PROJECT_DIR/demo.py +experiment="inference_demo" data_base_dir="$PROJECT_DIR/data/demo/demo_cam demo_cam-test" sfm_base_dir="$PROJECT_DIR/data/demo/sfm_model/outputs_softmax_loftr_loftr/demo_cam"
