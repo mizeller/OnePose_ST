692f638393426a3bdc3723ff3f9bfb9a71ef10f9
Michel Zeller <mizeller@student.ethz.ch>
2023-11-08 13:29:20 +0100

    chore: remove obsolete files

--- colmap-docker/build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t="colmap:cuda113" .;
-docker run --gpus all -w /workspaces/OnePose_ST -v ~/projects/OnePose_ST:/workspaces/OnePose_ST -it colmap:cuda113;
--- scripts/docker_root.sh
+++
@@ -1,7 +0,0 @@
-echo "Enabling non-root docker commands"
-echo "Should automatically docker run hello-world"
-
-chown root:docker /var/run/docker.sock
-chown -R root:docker /var/run/docker
-
-docker run hello-world
