a5ac74669ce200f74f555a693a04bfad6c6cbad3
Michel Zeller <mizeller@student.ethz.ch>
2023-11-09 15:49:53 +0000

    feat: custom inference script

--- inference.py
+++ inference.py
@@ -1,126 +1,173 @@
-from omegaconf.dictconfig import DictConfig
-import os
-from omegaconf import DictConfig
+from typing import List
+import os.path as osp
+from tqdm import tqdm
+from loguru import logger
 import yaml
+import demo
+from tqdm import tqdm
+from loguru import logger
+import os
 
-# important for DeepLM module, this line should before import torch
-os.environ["TORCH_USE_RTLD_GLOBAL"] = "TRUE"
+os.environ[
+    "TORCH_USE_RTLD_GLOBAL"
+] = "TRUE"  # important for DeepLM module, this line should before import torch
+import os.path as osp
 import numpy as np
 import torch
 
-import hydra
 from src.utils import data_utils
 from src.utils import vis_utils
-from src.utils.data_io import read_grayscale
-from src.inference.inference_OnePosePlus import build_model
 from src.utils.metric_utils import ransac_PnP
 from src.datasets.OnePosePlus_inference_dataset import OnePosePlusInferenceDataset
+from src.inference.inference_OnePosePlus import build_model
 from src.local_feature_object_detector.local_feature_2D_detector import (
     LocalFeatureObjectDetector,
 )
-import demo
 
 
-@hydra.main(config_path="configs/", config_name="config.yaml")
-def main(cfg: DictConfig):
-    globals()[cfg.type](cfg)
+####################################################################################################
+######################## TODO - MAKE YOUR MODIFICATIONS HERE M######################################
+####################################################################################################
+class CONFIG:
+    class DATAMODULE:
+        # this class will never be used outside of the CONFIG scope
+        load_3d_coarse: bool = True
+        shape3d_val: int = 7000
+        img_pad: bool = False
+        df: int = 8
+        pad3D: bool = False
+
+    def __init__(self):
+        self.project_root: str = "/workspaces/OnePose_ST"
+        self.data_root: str = f"{self.project_root}/data/spot/"
+        # NOTE: there needs to exist a sub-directory called "color_full" in ∀ data_dirs which contain the image sequences.
+        #       furthermore, an "instrinsics.txt" with the corresponding camera intrinsics is also required...
+        self.data_dirs: List[str] = ["spot-test"]  # extend list to incl. more data
+        self.sfm_model_dir: str = (
+            f"{self.data_root}/sfm_model/outputs_softmax_loftr_loftr/spot"
+        )
+        self.datamodule = CONFIG.DATAMODULE()
+        self.model: dict = self._get_model()
+
+    def _get_model(self) -> dict:
+        with open("configs/experiment/inference_demo.yaml", "r") as f:
+            onepose_config = yaml.load(f, Loader=yaml.FullLoader)
+        return onepose_config["model"]
 
 
-if __name__ == "__main__":
-    with open("configs/experiment/inference_demo.yaml", "r") as f:
-        onepose_config = yaml.load(f, Loader=yaml.FullLoader)
+####################################################################################################
+####################################################################################################
 
-    # TODO: fix these paths for your usecase
-    project_root: str = "/workspaces/OnePose_ST"
-    data_root: str = f"{project_root}/data/spot"
-    data_dir: str = f"{project_root}/data/spot/spot_01-annotate"
-    sfm_model_dir = (
-        f"{project_root}/data/spot/sfm_model/outputs_softmax_loftr_loftr/spot"
-    )
-    query_image_name = "test"
-    query_image_path = f"{project_root}/{query_image_name}.png"
 
-    img_list, paths = demo.get_default_paths(
-        {},
-        data_root=data_root,
-        data_dir=data_dir,
-        sfm_model_dir=sfm_model_dir,
-    )
-
-    local_feature_obj_detector = LocalFeatureObjectDetector(
-        sfm_ws_dir=paths["sfm_ws_dir"],
-        output_results=True,
-        detect_save_dir=paths["vis_detector_dir"],
-    )
-
-    match_2D_3D_model = build_model(
-        onepose_config["model"]["OnePosePlus"],
-        onepose_config["model"]["pretrained_ckpt"],
-    )
-    match_2D_3D_model.cuda()
-
+def inference_core(seq_dir):
+    """Inference core function for OnePosePlus. Adapted from demo.py"""
+    data_root = cfg.data_root
+    sfm_model_dir = cfg.sfm_model_dir
+    img_list, paths = demo.get_default_paths(data_root, seq_dir, sfm_model_dir)
     dataset = OnePosePlusInferenceDataset(
         paths["sfm_dir"],
         img_list,
-        load_3d_coarse=True,
-        shape3d=7000,
-        img_pad=False,
+        load_3d_coarse=cfg.datamodule.load_3d_coarse,
+        shape3d=cfg.datamodule.shape3d_val,
+        img_pad=cfg.datamodule.img_pad,
         img_resize=None,
-        df=8,
-        pad=False,
+        df=cfg.datamodule.df,
+        pad=cfg.datamodule.pad3D,
         load_pose_gt=False,
         n_images=None,
         demo_mode=True,
         preload=True,
     )
+    # NOTE: if you find pose estimation results are not good, problem maybe due to the poor object detection at the very beginning of the sequence.
+    # You can set `output_results=True`, the detection results will thus be saved in the `detector_vis` directory in folder of the test sequence.
+    local_feature_obj_detector: LocalFeatureObjectDetector = LocalFeatureObjectDetector(
+        sfm_ws_dir=paths["sfm_ws_dir"],
+        output_results=True,
+        detect_save_dir=paths["vis_detector_dir"],
+    )
+    match_2D_3D_model = build_model(
+        cfg.model["OnePosePlus"], cfg.model["pretrained_ckpt"]
+    )
+    match_2D_3D_model.cuda()
 
     K, _ = data_utils.get_K(paths["intrin_full_path"])
 
-    query_image, query_image_scale, query_image_mask = read_grayscale(
-        query_image_path,
-        resize=dataset.img_resize,
-        pad_to=None,
-        ret_scales=True,
-        ret_pad_mask=True,
-        df=dataset.df,
-    )
-    bbox, inp_crop, K_crop = local_feature_obj_detector.detect(
-        query_image, query_image_path, K
-    )
-    print(f"bbox: {bbox}")
-
-    input_data = {
-        "query_image": inp_crop.cuda(),
-        "query_image_path": query_image_path,
-        "descriptors3d_coarse_db": dataset.avg_coarse_descriptors3d[None],
-        "descriptors3d_db": dataset.avg_descriptors3d[None],
-        "keypoints3d": dataset.keypoints3d[None],
-    }
-
-    # Perform keypoint-free 2D-3D matching and then estimate object pose of query image by PnP:
-    with torch.no_grad():
-        match_2D_3D_model(input_data)
-    mkpts_3d = input_data["mkpts_3d_db"].cpu().numpy()  # N*3
-    mkpts_query = input_data["mkpts_query_f"].cpu().numpy()  # N*2
-    pose_pred, _, inliers, _ = ransac_PnP(
-        K_crop,
-        mkpts_query,
-        mkpts_3d,
-        scale=1000,
-        pnp_reprojection_error=7,
-        img_hw=[512, 512],
-        use_pycolmap_ransac=True,
-    )
-    print(f"pose_pred: {pose_pred}, inliers: {inliers}")
-    # Visualize:
     bbox3d = np.loadtxt(paths["bbox3d_path"])
-    vis_utils.save_demo_image(
-        pose_pred,
-        K,
-        image_path=query_image_path,
-        box3d=bbox3d,
-        draw_box=len(inliers) > 20,
-        save_path=f"outputs/pose_pred_{query_image_name}.jpg",
-    )
+    pred_poses = {}  # {id:[pred_pose, inliers]}
+    for id in tqdm(range(len(dataset))):
+        data = dataset[id]
+        query_image = data["query_image"]
+        query_image_path = data["query_image_path"]
 
-    print(f"Saved results to: {project_root}/outputs/pose_pred_{query_image_name}.jpg")
+        # Detect object:
+        if id == 0:
+            # Detect object by 2D local feature matching for the first frame:
+            _, inp_crop, K_crop = local_feature_obj_detector.detect(
+                query_image, query_image_path, K
+            )
+        else:
+            # Use 3D bbox and previous frame's pose to yield current frame 2D bbox:
+            previous_frame_pose, inliers = pred_poses[id - 1]
+
+            if len(inliers) < 20:
+                # Consider previous pose estimation failed, reuse local feature object detector:
+                _, inp_crop, K_crop = local_feature_obj_detector.detect(
+                    query_image, query_image_path, K
+                )
+            else:
+                (
+                    _,
+                    inp_crop,
+                    K_crop,
+                ) = local_feature_obj_detector.previous_pose_detect(
+                    query_image_path, K, previous_frame_pose, bbox3d
+                )
+
+        data.update({"query_image": inp_crop.cuda()})
+
+        # Perform keypoint-free 2D-3D matching and then estimate object pose of query image by PnP:
+        with torch.no_grad():
+            match_2D_3D_model(data)
+        mkpts_3d = data["mkpts_3d_db"].cpu().numpy()  # N*3
+        mkpts_query = data["mkpts_query_f"].cpu().numpy()  # N*2
+        pose_pred, _, inliers, _ = ransac_PnP(
+            K_crop,
+            mkpts_query,
+            mkpts_3d,
+            scale=1000,
+            pnp_reprojection_error=7,
+            img_hw=[512, 512],
+            use_pycolmap_ransac=True,
+        )
+
+        pred_poses[id] = [pose_pred, inliers]
+
+        # Visualize:
+        vis_utils.save_demo_image(
+            pose_pred,
+            K,
+            image_path=query_image_path,
+            box3d=bbox3d,
+            draw_box=len(inliers) > 20,
+            save_path=osp.join(paths["vis_box_dir"], f"{id}.jpg"),
+        )
+
+    # Output video to visualize estimated poses:
+    logger.info(f"Generate demo video begin...")
+    vis_utils.make_video(paths["vis_box_dir"], paths["demo_video_path"])
+
+
+def main() -> None:
+    # loop over all data_dirs specified in the CONFIG class
+    for test_dir in tqdm(cfg.data_dirs, total=len(cfg.data_dirs)):
+        seq_dir = osp.join(cfg.data_root, test_dir)
+        logger.info(f"Eval {seq_dir}")
+        inference_core(cfg, seq_dir)
+
+    logger.info("Done")
+
+
+if __name__ == "__main__":
+    global cfg
+    cfg: CONFIG = CONFIG()
+    main()
